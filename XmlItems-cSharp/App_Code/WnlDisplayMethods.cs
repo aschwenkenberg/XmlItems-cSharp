ff82fcd0ce98e083badc18e406e13fb088062288
Alexander Schwenkenberg <aschwenkenberg@omniupdate.com>
2012-11-26 15:46:58 -0800

    update work

--- XmlItems-cSharp/App_Code/WnlDisplayMethods.cs
+++ XmlItems-cSharp/App_Code/WnlDisplayMethods.cs
@@ -24,6 +24,72 @@ namespace OUC
             o += "</ul>";
             return o;
         }
+
+
+        public static string rss_sample_0(string _url, string _xpath)
+        {
+            LocalItems xml = new LocalItems(_url, _xpath);
+            string o = "<ul class=\"rss_hidelist\">";
+            xml.items.ForEach(x => { EtsuRssItem item = new EtsuRssItem(x); o += item.ToListSample0(); });
+            o += "</ul>";
+            return o;
+        }
+
+        public static string rss_sample_1(string _url, string _xpath)
+        {
+            LocalItems xml = new LocalItems(_url, _xpath);
+            string o = "<ul class=\"rss_simplelist\">";
+            xml.items.ForEach(x => { EtsuRssItem item = new EtsuRssItem(x); o += item.ToListSample1(); });
+            o += "</ul>";
+            return o;
+        }
+
+        public static string rss_sample_2(string _url, string _xpath)
+        {
+            LocalItems xml = new LocalItems(_url, _xpath);
+            string o = "<ul class=\"rss_simplelist\">";
+            xml.items.ForEach(x => { EtsuRssItem item = new EtsuRssItem(x); o += item.ToListSample2(); });
+            o += "</ul>";
+            return o;
+        }
+
+        public static string rss_sample_3(string _url, string _xpath)
+        {
+            LocalItems xml = new LocalItems(_url, _xpath);
+            string o = "<ul class=\"rss_simplelist\">";
+            xml.items.ForEach(x => { EtsuRssItem item = new EtsuRssItem(x); o += item.ToListSample3(); });
+            o += "</ul>";
+            return o;
+        }
+
+        public static string rss_sample_4(string _url, string _xpath)
+        {
+            LocalItems xml = new LocalItems(_url, _xpath);
+            string o = "<ul class=\"rss_hidelist\">";
+            xml.items.ForEach(x => { EtsuRssItem item = new EtsuRssItem(x); o += item.ToListSample4(); });
+            o += "</ul>";
+            return o;
+        }
+
+        public static string rss_sample_6(string _url, string _xpath)
+        {
+            LocalItems xml = new LocalItems(_url, _xpath);
+            string o = "<ul class=\"rss_hidelist\">";
+            xml.items.ForEach(x => { EtsuRssItem item = new EtsuRssItem(x); o += item.ToListSample6(); });
+            o += "</ul>";
+            return o;
+        }
+
+        public static string rss_sample_7(string _url, string _xpath)
+        {
+            LocalItems xml = new LocalItems(_url, _xpath);
+            string o = "<div style=\"overflow:hidden;\">";
+            xml.items.ForEach(x => { EtsuRssItem item = new EtsuRssItem(x); o += item.ToListSample7(); });
+            o += "</div>";
+            return o;
+        }
+       
+
        
         public static string eventsList(string _url,string _xpath)
         {
