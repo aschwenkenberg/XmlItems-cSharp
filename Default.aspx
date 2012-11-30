f94bf5b7e7e42e2d5fbc71d39e5d39b5ad253881
Alexander Schwenkenberg <aschwenkenberg@omniupdate.com>
2012-11-30 15:27:17 -0800

    ajax call

--- Default.aspx
+++ Default.aspx
@@ -3,14 +3,49 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
+<head runat="server">    
+    <!--<link href="css/OUpage_layout_general.css" rel="Stylesheet" type="text/css" />-->
+    <script type="text/javascript" src='js/jquery-1.8.3.min.js' />
+    <script type="text/javascript" src='js/rss_news.js' />
+    <title>WNL Display Methods</title>
+
 </head>
 <body>
     <form id="form1" runat="server">
     <div id="cont" runat="server">
     
     </div>
+    
+
+<script type="text/javascript">
+
+        var ajaxUrl = "wnl_handler.ashx";
+        $(document).ready(function () {
+
+            var quantity = 3;
+            var display = "rss_sample_2";
+            var feed = "rss/news2012.xml";
+            var xpath = "rss/channel/item";
+            var type = "test&data";
+
+            var param = { display:display, feed:feed, xpath:xpath, quantity:quantity };
+
+            $.ajax({
+                url: ajaxUrl,
+                data: param,
+                async: true,
+                contentType: "text/plain",
+                success: function (data) {
+                    $("#news-list").html(data);
+                }
+            })
+
+        });
+
+       
+</script>
+
+ <div id="news-list"><!-- no empty elements --></div>
     </form>
 </body>
 </html>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,15 +11,24 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         //cont.InnerHtml = WnlDisplayMethods.newsList("http://www.gallenauniversity.com/_resources/rss/news.xml","/rss/channel/item");
-        cont.InnerHtml += WnlDisplayMethods.rss_sample_7("rss/news2012.xml", "/rss/channel/item",10);
-        cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_0");
-        cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_1");
-        cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_2");
-        cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_3");
-        cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_4");
-        cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_6");
-        cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_7");
+        //cont.InnerHtml += WnlDisplayMethods.rss_sample_7("rss/news2012.xml", "/rss/channel/item",10);
+        //cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_0");
+        //cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_1");
+        //cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_2");
+        //cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_3");
+        //cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_4");
+        //cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_6");
+        //cont.InnerHtml += WnlDisplayMethods.MasterDisplayer("rss/news2012.xml", "/rss/channel/item", 3, "rss_sample_7");
         //cont.InnerHtml += WnlDisplayMethods.eventsList("http://calendar.spsu.edu/webcache/v1.0/xmlDays/7/list-xml/no--filter.xml", "/bedework/events/event");
         
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+        Page.ClientScript.RegisterClientScriptInclude(
+            "JQueryInclude",
+            "js/jquery-1.8.3.min.js");
+        
+        base.OnLoad(e);
     }
 }
